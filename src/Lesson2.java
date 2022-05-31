e0e64a3c246e9edef47cf61c2794e8d9962710fd
Kiyuanochka <93634306+Kiyuanochka@users.noreply.github.com>
2022-05-31 19:29:57 +0700

    Add files via upload

---
+++ src/Lesson2.java
@@ -0,0 +1,43 @@
+public class Lesson2 {
+    public static void main(String[] args) {
+        //numbers();
+        //numbers2();
+        //Line();
+        boolean isLimit = true;
+        int a = 5;
+        int b = 19;
+        if (10 >= (a + b) && (a + b) <= 20) ;
+    }
+    /*public static void numbers() {*/
+
+
+
+    /*public static void numbers2() {
+        int с = 5;
+
+        if (с >= 0) {
+            System.out.println("Положительное");
+        } else {
+            System.out.println("Отрицательное");
+        }
+    }
+    public static void numbers3() {
+        int d = 5;
+
+        if (d >= 0) {
+            System.out.println("False");
+        } else {
+            System.out.println("True");
+        }
+    }
+    public static void Line() {
+        String line = "Hello";
+
+        for(int number = 0; number <= 5; number++);
+        {
+            System.out.println(line);
+        }
+    }
+
+     */
+    }
